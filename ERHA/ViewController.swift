490412cf67e7cf168bd22879c867f83641390d11
Emiaostein <cszwdy@gmail.com>
2018-02-24 19:01:51 +0800

    1. Dispatch group.

--- ERHA/ViewController.swift
+++ ERHA/ViewController.swift
@@ -18,42 +18,82 @@ class ViewController: UIViewController {
         super.viewDidLoad()
     }
 
-    @IBAction func action_click(_ sender: Any) {
-        method()
-    }
-    
-    func method() {
-        queue0.async {[weak self] in
+    @IBAction func action_success(_ sender: Any) {
+        methodSuccess?.cancel()
+        let item = DispatchWorkItem { print("success!") }
+        methodSuccess = item
+        let timeoutItem = DispatchWorkItem { print("timetout") }
+
+        run(concurrent: {[weak self] (group) in
             guard let sf = self else {return}
-            sf.methodSuccess?.cancel()
-            
-            let group = DispatchGroup()
-            
+            group.enter()
+            sf.queue1.asyncAfter(deadline: .now()+1, execute: {
+                group.leave()
+            })
+
+            print("start")
+
             group.enter()
             sf.queue1.asyncAfter(deadline: .now()+2, execute: {
                 group.leave()
             })
-            
-            print("start")
-            
+        }, timeout: .now() + 3, success: item, timeoutItem: timeoutItem)
+        
+    }
+    
+    @IBAction func action_done(_ sender: Any) {
+        guard let  success = methodSuccess, success.isCancelled == false else {
+            methodSuccess = nil
+            return
+        }
+        success.perform()
+        success.cancel()
+    }
+    
+    @IBAction func action_timeout(_ sender: Any) {
+        
+        methodSuccess?.cancel()
+        
+        let item = DispatchWorkItem {
+            print("success!")
+        }
+        
+        methodSuccess = item
+        
+        let timeoutItem = DispatchWorkItem {
+            print("timetout")
+        }
+        
+        run(concurrent: {[weak self] (group) in
+            guard let sf = self else {return}
             group.enter()
-            sf.queue1.asyncAfter(deadline: .now()+4, execute: {
-                group.leave()
-            })
-            let success = DispatchWorkItem {
-                print("success")
-            }
-            sf.methodSuccess = success
-            let result = group.wait(timeout: .now()+10)
-            
+            sf.queue1.asyncAfter(deadline: .now()+1, execute: { group.leave() })
+            print("start")
+            group.enter()
+            sf.queue1.asyncAfter(deadline: .now()+2, execute: { group.leave() })
+            },
+            timeout: .now() + 1.5,
+            success: item,
+            timeoutItem: timeoutItem)
+    }
+    
+    
+    func run(concurrent:@escaping (DispatchGroup)->(), timeout: DispatchTime, success: DispatchWorkItem, timeoutItem: DispatchWorkItem? = nil) {
+        queue0.async {
+            let group = DispatchGroup()
+            concurrent(group)
+            let result = group.wait(timeout: timeout)
             switch result {
+            case .success:
+                if success.isCancelled == false {
+                    group.notify(queue: DispatchQueue.main, work: success)
+                }
             case .timedOut:
                 success.cancel()
-            default:
-                ()
+                if let t = timeoutItem, t.isCancelled == false {
+                    group.notify(queue: DispatchQueue.main, work: t)
+                }
             }
-            
-            group.notify(queue: DispatchQueue.main, work: success)
         }
     }
 }
